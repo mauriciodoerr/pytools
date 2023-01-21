d24e6bd9acad0ea1cf32fded227dd18505a3569c
Mauricio Doerr <mauricio.doerr@gmail.com>
2023-01-21 20:22:23 -0300

    Adjusting PyPi setup for Python3 print function

--- setup.py
+++ setup.py
@@ -70,9 +70,7 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                    "Directory %s ignored by pattern %s"
-                                    % (fn, pattern))
+                            print(f'Directory {fn} ignored by pattern {pattern}', file=sys.stderr)
                         break
                 if bad_name:
                     continue
@@ -93,9 +91,7 @@ def find_package_data(
                             or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print >> sys.stderr, (
-                                    "File %s ignored by pattern %s"
-                                    % (fn, pattern))
+                            print(f'File {fn} ignored by pattern {pattern}', file=sys.stderr)
                         break
                 if bad_name:
                     continue
