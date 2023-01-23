6a7b122ce8ced2d9d58ea57942a3b2bd5988ab9e
Mauricio Doerr <mauricio.doerr@gmail.com>
2023-01-23 16:27:08 -0300

    Mocking integrated github API test with pytest fixture within setup and tear down.

--- libhttprequests/tests/test_github_api.py
+++ libhttprequests/tests/test_github_api.py
@@ -1,16 +1,31 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libhttprequests import github_api
 from libhttprequests.github_api import get_github_avatar
 
 
-def test_github_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/28199933?v=4'
     resp_mock.json.return_value = {
-        "login": "mauriciodoerr",
-        "id": 28199933,
-        "avatar_url": "https://avatars.githubusercontent.com/u/28199933?v=4"
+        'login': 'mauricio',
+        'id': 28199933,
+        'avatar_url': url
     }
+    github_original_api = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = github_original_api
+
+
+def test_github_avatar(avatar_url):
+    result = get_github_avatar('mauriciodoerr')
+    assert '28199933' in result
+
+
+def test_github_avatar_integrated():
     result = get_github_avatar('mauriciodoerr')
     assert '28199933' in result
