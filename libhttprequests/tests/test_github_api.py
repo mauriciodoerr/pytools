05e2df1e7ef74904ddfa22b9d76b0c94c7145b5f
Mauricio Doerr <mauricio.doerr@gmail.com>
2023-01-23 16:35:50 -0300

    Replacing Mock with pytest-mock when setting up and performing tear down for github api integrated test.

--- libhttprequests/tests/test_github_api.py
+++ libhttprequests/tests/test_github_api.py
@@ -1,31 +1,28 @@
 from unittest.mock import Mock
 
 import pytest
-
-from libhttprequests import github_api
 from libhttprequests.github_api import get_github_avatar
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
-    url = 'https://avatars.githubusercontent.com/u/28199933?v=4'
+    url = 'mocked url'
     resp_mock.json.return_value = {
         'login': 'mauricio',
         'id': 28199933,
         'avatar_url': url
     }
-    github_original_api = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libhttprequests.github_api.requests.get')
+    get_mock.return_value = resp_mock
     yield url
-    github_api.requests.get = github_original_api
 
 
 def test_github_avatar(avatar_url):
     result = get_github_avatar('mauriciodoerr')
-    assert '28199933' in result
+    assert 'mocked url' in result
 
 
 def test_github_avatar_integrated():
     result = get_github_avatar('mauriciodoerr')
-    assert '28199933' in result
+    assert 'https://avatars.githubusercontent.com/u/28199933?v=4' in result
