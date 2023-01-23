26c739a830cc5384e14a4bb3a00ab68162569235
Mauricio Doerr <mauricio.doerr@gmail.com>
2023-01-23 14:36:27 -0300

    Setup and Tear Down for pytests.

--- libhttprequests/tests/test_spam/test_users.py
+++ libhttprequests/tests/test_spam/test_users.py
@@ -1,25 +1,36 @@
+import pytest
+
 from libhttprequests.spam.db import DBConnection
 from libhttprequests.spam.models import User
 
 
-def test_save_user():
-    connection = DBConnection()
-    session = connection.generate_session()
+@pytest.fixture
+def connection():
+    # Setup
+    db_connection = DBConnection()
+    # Tear Down
+    yield db_connection
+    db_connection.close()
+
+
+@pytest.fixture
+def session(connection):
+    # Setup
+    db_session = connection.generate_session()
+    yield db_session
+    # Tear Down
+    db_session.roll_back()
+    db_session.close()
+
+
+def test_save_user(session):
     user = User(name='User One')
     session.save(user)
     assert isinstance(user.id, int)
-    session.roll_back()
-    session.close()
-    connection.close()
 
 
-def test_list_users():
-    connection = DBConnection()
-    session = connection.generate_session()
+def test_list_users(session):
     users = [User(name='User One'), User(name='User Two')]
     for user in users:
         session.save(user)
     assert users == session.list()
-    session.roll_back()
-    session.close()
-    connection.close()
